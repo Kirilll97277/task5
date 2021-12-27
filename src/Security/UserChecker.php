1cb322fee802f4b060d92e36b0c3a1bc1b602797
Kirill Kuzhminich <kirill19970806@gmail.com>
2021-12-27 10:34:44 +0300

    logout on uninstall

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -2,6 +2,9 @@
 
 namespace App\Security;
 
+
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -9,8 +12,12 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
-    public function checkPreAuth(UserInterface $user)
+    public function checkPreAuth(UserInterface $user): void
     {
+        if(!$user instanceof AppUser){
+            return;
+        }
+
         if ($user->getIsActive() == false) {
             throw new CustomUserMessageAuthenticationException("You're banned !");
         }
@@ -18,6 +25,12 @@ class UserChecker implements UserCheckerInterface
 
     public function checkPostAuth(UserInterface $user)
     {
+        if(!$user instanceof AppUser){
+            return;
+        }
 
+        if ($user->getIsActive() == false) {
+            throw new CustomUserMessageAuthenticationException("You're banned 1!");
+        }
     }
 }
